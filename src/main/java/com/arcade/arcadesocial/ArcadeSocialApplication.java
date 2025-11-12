efa2f21339dfbecd3753a09431ecf8647b79a2ea
alireza <theethanwardhere@gmail.com>
2025-11-12 20:49:47 +0330

    first commit

---
+++ src/main/java/com/arcade/arcadesocial/ArcadeSocialApplication.java
@@ -0,0 +1,13 @@
+package com.arcade.arcadesocial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArcadeSocialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArcadeSocialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arcade/arcadesocial/ArcadeSocialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arcade.arcadesocial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArcadeSocialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
